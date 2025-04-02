5de8ebb1e742149d5c25f2f9754ca691e49b8cb6
Pierrick Naessens <pierrick.naessens62@gmail.com>
2025-04-02 10:35:25 +0200

    add routine meal  and death for fork

--- bonus/src/monitor_bonus.c
+++ bonus/src/monitor_bonus.c
@@ -6,7 +6,7 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 09:40:32 by pnaessen          #+#    #+#             */
-/*   Updated: 2025/04/01 17:27:00 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2025/04/02 10:34:42 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,36 @@ void	*monitor_routine(void *arg)
 	return (NULL);
 }
 
+void	*death_routine(void *arg)
+{
+	t_wait	*wait_data;
+
+	wait_data = (t_wait *)arg;
+	sem_wait(wait_data->data->stop);
+	pthread_mutex_lock(wait_data->lock);
+	*wait_data->simulation_end = true;
+	pthread_mutex_unlock(wait_data->lock);
+	return (NULL);
+}
+
+void	meals_routine(t_wait *arg)
+{
+	t_wait	*wait_data;
+	int		i;
+
+	wait_data = (t_wait *)arg;
+	i = 0;
+	while (i < wait_data->data->num_philos)
+	{
+		sem_wait(wait_data->data->finished);
+		pthread_mutex_lock(wait_data->lock);
+		(*wait_data->meals_eaten)++;
+		pthread_mutex_unlock(wait_data->lock);
+		i++;
+	}
+	return (NULL);
+}
+
 void	wait_for_processes(t_data *data)
 {
 	pthread_t		death_thread;
@@ -38,7 +68,9 @@ void	wait_for_processes(t_data *data)
 	bool			death;
 	int				meals_eaten;
 	pthread_mutex_t	mutex;
+	int				i;
 
+	i = 0;
 	death = false;
 	meals_eaten = 0;
 	pthread_mutex_init(&mutex, NULL);
@@ -47,12 +79,21 @@ void	wait_for_processes(t_data *data)
 	wait_data.lock = &mutex;
 	wait_data.meals_eaten = &meals_eaten;
 	// init_wait(&wait_data, data, &death, &meals_eaten);
-	pthread_create(&death_thread, NULL, , &wait_data);
+	pthread_create(&death_thread, NULL, death_routine, &wait_data);
 	if (data->max_meals > 0)
-		pthread_create(&meals_thread, NULL, , &wait_data);
+		pthread_create(&meals_thread, NULL, meals_routine, &wait_data);
 	while (1)
 	{
+		pthread_mutex_lock(&mutex);
+		if (death || (data->max_meals > 0 && meals_eaten >= data->num_philos))
+		{
+			pthread_mutex_unlock(&mutex);
+			break ;
+		}
+		pthread_mutex_unlock(&mutex);
+		usleep(1000);
 	}
+	while()
 }
 
 // void	init_wait(t_wait *wait_data, t_data *data, bool death, int meals_eaten)
